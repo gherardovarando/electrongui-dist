b153f8de797f1a39eefdb1347ce95aa58835bd33
gherardovarando <gherardo.varando@gmail.com>
2017-11-10 14:17:28 +0100

    initail

---
+++ app/main.js
@@ -0,0 +1,119 @@
+// Copyright (C) 2016  Gherardo Varando (gherardo.varando@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+const {
+  fork,
+  exec
+} = require('child_process')
+const {
+  app,
+  BrowserWindow,
+  Tray,
+  Menu,
+  dialog,
+  shell
+} = require('electron')
+const path = require('path')
+const {
+  ipcMain
+} = require('electron')
+
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+let trayimg = `${__dirname}/icon.png`
+if (process.platform === 'win32') {
+  trayimg = `${__dirname}/icon.ico`
+}
+
+
+
+
+function createWindow() {
+
+  let frame = true;
+  if (process.platform === "linux" | process.platform === "darwin") {
+    frame = true;
+  }
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: frame,
+    titleBarStyle: 'hidden',
+    icon: `${__dirname}/icon.png`
+  })
+
+  // and load the index.html of the app.
+  win.loadURL(`file://${__dirname}/index.html`)
+
+  // Open the DevTools.
+  //win.webContents.openDevTools()
+  win.webContents.on('will-navigate', (e, url) => {
+    if (url != win.webContents.getURL()) {
+      e.preventDefault()
+      shell.openExternal(url)
+    }
+  })
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+
+
+app.commandLine.appendSwitch("disable-renderer-backgrounding");
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+
+
+//Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createMainWindow()
+  }
+})
+
+ipcMain.on("focusWindow", () => {
+  win.focus();
+})
+
+ipcMain.on("openDevTools", () => {
+  win.webContents.openDevTools()
+})
+
+ipcMain.on('setProgress', (event, arg) => {
+  win.setProgressBar(arg.value);
+});
---
+++ app/mainWindow.js
@@ -0,0 +1,154 @@
+// Copyright (C) 2017  Gherardo Varando (gherardo.varando@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+'use strict'
+
+let t = new Date()
+const {
+  Menu,
+  MenuItem,
+  app
+} = require('electron').remote
+const isSecond = app.makeSingleInstance((argv, workingdir) => {
+  if (argv.includes('--clean')) gui.workspace.newChecking()
+  if (win) win.show()
+})
+if (isSecond) {
+  app.quit()
+}
+const isDev = require('electron-is-dev')
+const {
+  TasksViewer,
+  Gui,
+  util
+} = require(`electrongui`)
+
+
+let mainProc = require('electron').remote.require('process')
+let isCleanW = mainProc.argv.includes('--clean')
+
+let gui = new Gui({
+  workspace: {
+    clean: isCleanW
+  }
+})
+gui.hide()
+
+
+//prevent app closing
+document.addEventListener('dragover', function(event) {
+  event.preventDefault()
+  return false
+}, false)
+
+document.addEventListener('drop', function(event) {
+  event.preventDefault()
+  return false
+}, false)
+
+
+if (isDev) {
+  gui.addMenuItem(new MenuItem({
+    label: "Dev",
+    type: 'submenu',
+    submenu: Menu.buildFromTemplate([{
+      label: 'toggledevtools',
+      click(item, focusedWindow) {
+        if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+      }
+    }])
+  }))
+}
+
+gui.addMenuItem(new MenuItem({
+  label: "File",
+  type: "submenu",
+  submenu: Menu.buildFromTemplate([{
+      label: 'New Workspace',
+      click: () => {
+        gui.workspace.newChecking()
+      }
+    },
+    {
+      label: 'Open Workspace',
+      click: () => {
+        gui.workspace.loadChecking()
+      }
+    },
+    {
+      label: 'Save Workspace',
+      click: () => {
+        gui.workspace.save(gui.workspace.spaces.workspace.path)
+      }
+    },
+    {
+      label: 'Save Workspace as',
+      click: () => {
+        gui.workspace.save()
+      }
+    },
+    {
+      label: 'Quit',
+      role: 'quit'
+    }
+  ])
+}))
+
+gui.header.actionsContainer.addButton({
+  id: 'save',
+  groupId: 'basetools',
+  icon: 'fa fa-save',
+  title: 'Save current workspace',
+  action: () => {
+    gui.workspace.save()
+  }
+})
+
+
+gui.header.actionsContainer.addButton({
+  id: 'load',
+  groupId: 'basetools',
+  icon: 'fa fa-folder-open-o',
+  title: 'Load a workspace',
+  action: () => {
+    gui.workspace.loadChecking()
+  }
+})
+
+gui.workspace.on('load', (e) => {
+  gui.win.setTitle(`${gui.workspace.spaces.workspace.path}--${app.getName()}-v${app.getVersion()}`)
+})
+gui.workspace.on('save', (e) => {
+  gui.win.setTitle(`${gui.workspace.spaces.workspace.path}--${app.getName()}-v${app.getVersion()}`)
+})
+
+gui.workspace.on('error', (e) => {
+  gui.alerts.add(e.message, 'warning')
+})
+
+gui.extensions.on('error', (e) => {
+  gui.alerts.add(e.message, 'error')
+  throw e
+})
+
+gui.extensions.activate() //activate the extensionmanager
+let tasksViewer = new TasksViewer(gui)
+tasksViewer.activate()
+
+gui.viewTrick()
+let stat = 'default'
+if (isDev) stat = 'warning'
+gui.alerts.add(`App loaded in ${(new Date())-t} ms`, stat)
+module.exports = gui
